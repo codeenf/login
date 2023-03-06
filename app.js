e2e4ee13b843f73b827e28ef744783f131a81f8f
sahiltyagi <119033461+codeenf@users.noreply.github.com>
2023-03-06 21:12:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/" , function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/" , function(req ,res){
+   const name = req.body.fn
+   const email = req.body.mail
+
+ if(name==='sahil' && email ==='sahil@123'){
+    res.sendFile(__dirname +'/index2.html')
+ }else{
+    res.send('Authentication failed')
+ }
+})
+
+app.listen(3000 , function(){
+    console.log("server is running on port 3000");
+})
